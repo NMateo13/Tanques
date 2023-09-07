e943825e42be099f3275b0276c9a35c5efbfe5bd
Bastián Espinoza <bespinoza20@alumnos.utalca.cl>
2023-09-06 23:08:11 -0300

    Se agregan png's de balas y explosión

--- Pantalla.py
+++ Pantalla.py
@@ -16,7 +16,7 @@ screen = pygame.display.set_mode(size)
 while True:
     for event in pygame.event.get(): #Comienza a capturar todo evento que suceda en la pantalla
 
-        #print(event) linea de codigo que nos servira para saber que eventos ocurren en la pantalla 
+        print(event) #linea de codigo que nos servira para saber que eventos ocurren en la pantalla
 
         if event.type == pygame.QUIT: #Cierra la pantalla al presionar el cerrar
             sys.exit()
