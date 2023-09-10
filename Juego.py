79a28467932e1595753acfd465110b26b22658ee
Nicole Pereira <npereira20@alumnos.utalca.cl>
2023-09-10 18:22:54 -0300

    Se maneja la pantalla como matriz

--- Juego.py
+++ Juego.py
@@ -1,4 +1,4 @@
-import Pantalla, pygame, sys
+import Pantalla, pygame, sys, Terreno
 
 pygame.init()
 
@@ -6,14 +6,29 @@ size = (Pantalla.pantalla.ancho, Pantalla.pantalla.alto)
 screen = pygame.display.set_mode(size)
 
 while True:
-    for event in pygame.event.get(): #Comienza a capturar todo evento que suceda en la pantalla
+    for event in pygame.event.get(): 
 
         #print(event) linea de codigo que nos servira para saber que eventos ocurren en la pantalla 
 
-        if event.type == pygame.QUIT: #Cierra la pantalla al presionar el cerrar
+        if event.type == pygame.QUIT: 
             sys.exit()
 
     screen.fill(Pantalla.pantalla.WHITE)
+
+
+    #Zona de programación
+
+    Pantalla.pantalla.crearMatriz(Pantalla.pantalla.alto, Pantalla.pantalla.ancho)
+    Terreno.terreno.genTerreno(Terreno.terreno.Xpos, Terreno.terreno.Ypos)
+    Pantalla.pantalla.dibujar(screen)
+
+    #Rectángulo con medidas del HUD
+    pygame.draw.rect(screen, Pantalla.pantalla.GRAY, (0, 540, 1200, 120))
+
+
+    #Zona de programación
+
+
     pygame.display.flip()
 
 
--- Pantalla.py
+++ Pantalla.py
@@ -1,24 +1,33 @@
-import pygame, sys 
+import pygame
 
 class Pantalla:
 
     WHITE = (225, 225, 225)
     BLACK = (0, 0, 0)
+    GREEN = (0, 153, 0)
+    GRAY = (128, 139, 150)
 
+    medidaHUD = 120
 
     def __init__(self, ancho, alto):
         
         self.ancho = ancho
         self.alto = alto
-        self.size = []
 
-pantalla = Pantalla(800, 500)
-print(pantalla.ancho, pantalla.alto)
-#size = [pantalla.ancho, pantalla.alto]
     
-    ###Zona de dibujo o programacion 
+    def crearMatriz(self, alto, ancho):
 
-    #(inicio, final, salto)
+        Pantalla.matriz = [[0] * ancho for _ in range(alto - Pantalla.medidaHUD)]
 
+    def dibujar(self, screen):
+        
+        for filas in range(0, (pantalla.alto - pantalla.medidaHUD)):
+            for columnas in range (0, pantalla.ancho):
+                if pantalla.matriz[filas][columnas] == 1:
+                    pygame.draw.rect(screen, pantalla.GREEN, (columnas, filas, 6, 6))
 
-    ##Zona de dibujo o programacion
+        
+pantalla = Pantalla(1200, 660)
+matriz = []
+
+    
