cc08aae26b6ecaedd7d4e6004e659f2651d194ed
Bastián Espinoza <bespinoza20@alumnos.utalca.cl>
2023-09-20 21:48:33 -0300

    Cambios en la interfaz gráfica

--- Juego.py
+++ Juego.py
@@ -3,7 +3,6 @@ from Bala import Bala
 pygame.init()
 
 current_time = 0
-RED = (255, 0 , 0)
 font = pygame.font.Font(None, 36)
 size = (Pantalla.pantalla.ancho, Pantalla.pantalla.alto)
 screen = pygame.display.set_mode(size)
@@ -11,7 +10,7 @@ FPS = 60
 Clock = pygame.time.Clock()
 disparo = Bala(300,350,20,100,9.8,0.5)
 pygame.display.set_caption("Juego de Tanques")
-pygame.display.set_icon(Pantalla.pantalla.Explosion)
+pygame.display.set_icon(Pantalla.pantalla.IMG_Explosion)
 
 while True:
     Clock.tick(FPS) #Controla la velocidad de la pantalla (FPS)
@@ -24,18 +23,18 @@ while True:
 
     screen.fill(Pantalla.pantalla.WHITE)
     screen.blit(Pantalla.pantalla.Background, (0, 0))
-    pygame.draw.rect(screen, Pantalla.pantalla.GRAY, (0, 540, 1200, 120))
+    screen.blit(Pantalla.pantalla.HUD, (0, 540))
 
     #Zona de programación
-    disparo.verificacion(current_time,1200,600,screen,RED)
+    disparo.verificacion(current_time,1200,600,screen,Pantalla.pantalla.BLACK)
     Pantalla.pantalla.crearMatriz(Pantalla.pantalla.alto, Pantalla.pantalla.ancho)
     Terreno.terreno.genTerreno(Terreno.terreno.Xpos, Terreno.terreno.Ypos)
     Pantalla.pantalla.dibujar(screen)
     Pantalla.pantalla.muestra_salud(screen, font)
     Pantalla.pantalla.muestra_potencia(screen, font)
     Pantalla.pantalla.muestra_angulo(screen, font)
-    Pantalla.pantalla.muestra_jugador(screen, font)
-    Pantalla.pantalla.muestra_tanques(screen)
+    Pantalla.pantalla.muestra_texto(screen, font)
+    Pantalla.pantalla.muestra_imagen(screen)
     current_time += disparo.incremento
 
     #Zona de programación
